2f50a6b2835a96dc16ecf9a647d139558b9f5fdd
Md. Shayed Hasan <80702278+shayedhasan@users.noreply.github.com>
2022-03-01 23:34:15 +0600

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+console.log('My name is Shayed Hasan')
+console.log('I love Bangladesh')
+console.log(55.69)
+
+console.log('My favourite number is: ' + 9)
+
+console.log(9+9)
+console.log('i am chacking that if there is any chance to make 9 + 9 = ' + 18)
+console.log(18 + 9)
---
+++ Variable_JS.js
@@ -0,0 +1,8 @@
+var name = ' Md. Shayed Hasan'
+var age = 26
+
+console.log(name)
+console.log(age)
+
+console.log('My name is ' + name)
+console.log('and my age is ' + age)   
